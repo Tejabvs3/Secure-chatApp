f2934b82569927344bf86e79a41b6ad230672d58
tejabvs <tejabvs3@gmail.com>
2024-12-17 17:00:49 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -21,14 +21,9 @@ require('./connection');
 const port = process.env.PORT_BACKEND
 // console.log(process.env.PORT_FRONTEND)
 const io = new Server(server, {
-    // cors: {
-    //     //origin: `http://localhost:${process.env.PORT_FRONTEND}`, //changed from 3000
-    //     origin: process.env.URL_FRONTEND,
-    //     methods: ['GET', 'POST']
-    // }
     cors: {
-      origin: '*',  // WARNING: Allowing all origins is insecure!
-      methods: ['GET', 'POST']
+        origin: process.env.URL_FRONTEND,
+        methods: ['GET', 'POST']
     }
 });
 
